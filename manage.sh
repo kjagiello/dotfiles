9f06ef6c9c21280b45a3adeda6d4a507fd9ec5bd
Krzysztof Jagiello <me@kjagiello.com>
2019-06-11 20:46:13 +0200

    Improve command output

--- manage.sh
+++ manage.sh
@@ -15,17 +15,17 @@ usage() {
 
 dot__install() {
   [ -z $1 ] && echo "Usage: install [module]" && exit
-  [ ! -d "./${1}" ] && echo "ERROR: Module "${1}" not found" && exit
+  [ ! -d "./${1}" ] && echo "ERROR: Module \"${1}\" not found" && exit
   [ -f "./${1}/.init.sh" ] && "./${1}/.init.sh"
   stow --ignore '\.init\.sh' $1
-  echo "Module "${1}" has been successfully installed."
+  echo "Module \"${1}\" has been successfully installed."
 }
 
 dot() {
   if type "dot__$1" >/dev/null 2>&1; then
     "dot__$1" "${@:2}"
   else
-    echo "ERROR: Command $1 not found."
+    echo "ERROR: Command \"$1\" not found."
     echo ""
     usage
   fi
