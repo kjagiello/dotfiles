bc9d5c8e82f4751387244d1ee8e7888cc75363b3
Krzysztof Jagiello <me@kjagiello.com>
2019-06-13 14:06:36 +0200

    Add fisher and nvm to fish

---
+++ fish/.init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+brew install fish fzf
+
+# Install and activate fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fish -c fisher
+
+fish -c 'fisher add jorgebucaran/fish-nvm'
