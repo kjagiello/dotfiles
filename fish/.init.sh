9742d1852935d5ba3488cd4e7126e08ae0d4be2d
Krzysztof Jagiello <me@kjagiello.com>
2019-07-25 08:36:11 +0200

    Add some new utils

--- fish/.init.sh
+++ fish/.init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-brew install fish fzf bat tokei jq bat up
+brew install fish fzf bat tokei jq jo bat up entr
 
 # Install and activate fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
