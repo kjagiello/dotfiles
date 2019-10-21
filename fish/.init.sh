92764c64f3bb9e3834a032cd094baf14ca443d2d
Krzysztof Jagiello <me@kjagiello.com>
2019-10-21 08:25:00 +0200

    Add new tools

--- fish/.init.sh
+++ fish/.init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-brew install coreutils gnu-sed fish fzf bat tokei jq jo bat up entr ripgrep
+brew install coreutils gnu-sed fish fzf bat tokei jq jo bat up entr ripgrep hadolint git-crypt
 
 # Install and activate fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
