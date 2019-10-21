aa85481bd54c1106a70f778948b8a7a7c6fa9864
Krzysztof Jagiello <me@kjagiello.com>
2019-10-21 08:25:26 +0200

    Improve the prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,19 +1,22 @@
 function fish_prompt
+    printf "%s in %s" \
+        (set_color yellow)(whoami)(set_color normal) \
+        (set_color $fish_color_cwd)(pwd)(set_color normal)
 
-   printf "%s in %s" \
-      (set_color yellow)(whoami)(set_color normal) \
-      (set_color $fish_color_cwd)(pwd)(set_color normal)
+    # Line 2
+    echo
+    if test $VIRTUAL_ENV
+        printf "(%s:%s) " \
+           (set_color cyan)(echo "venv")(set_color normal) \
+           (set_color normal)(basename (dirname $VIRTUAL_ENV))(set_color normal)
+    end
+    if test $DOCKER_HOST
+        printf "[%s] " (set_color red)(echo $DOCKER_STACK)(set_color normal)
+    end
+    printf "\$ "
+    set_color normal
+end
 
-   # Line 2
-   echo
-   if test $VIRTUAL_ENV
-       printf "(%s:%s) " \
-          (set_color cyan)(echo "venv")(set_color normal) \
-          (set_color normal)(basename (dirname $VIRTUAL_ENV))(set_color normal)
-   end
-   if test $DOCKER_HOST
-       printf "[%s] " (set_color red)(echo $DOCKER_STACK)(set_color normal)
-   end
-   printf "↪ "
-   set_color normal
+function fish_right_prompt
+    #intentionally left blank
 end
