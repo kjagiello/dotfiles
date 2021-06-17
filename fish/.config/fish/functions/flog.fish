9fe3753693cbb595b4bcd9a88b06e9173bc706a7
Krzysztof Jagiello <me@kjagiello.com>
2021-06-17 15:17:36 +0200

    Add support for selecting multiple commits

--- fish/.config/fish/functions/flog.fish
+++ fish/.config/fish/functions/flog.fish
@@ -1,3 +1,3 @@
 function flog --description 'Fuzzy git log search that outputs the selected hash'
-  git log --color=always --decorate --oneline | fzf --ansi --reverse | awk '{ print $1 }'
+  git log --color=always --decorate --oneline | fzf --ansi --reverse --multi | awk '{ print $1 }' | tr '\n' ' '
 end
